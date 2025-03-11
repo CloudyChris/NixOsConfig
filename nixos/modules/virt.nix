376f3f49e7887f588ca326d43100ddb63b384830
CloudyChris <a.c.pufu@gmail.com>
2025-03-11 08:08:28 +0200

    added quickgui - gui for quickemu

--- nixos/modules/virt.nix
+++ nixos/modules/virt.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     qemu
     quickemu
+    quickgui
   ];
 
   systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
