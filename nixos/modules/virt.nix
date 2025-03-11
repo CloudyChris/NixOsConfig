f6e9ab0a91d3747b5cd1101759d3ed10d47eb338
CloudyChris <a.c.pufu@gmail.com>
2025-03-11 07:42:21 +0200

    added quickemu because I'm not a masochist

--- nixos/modules/virt.nix
+++ nixos/modules/virt.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [qemu];
+  environment.systemPackages = with pkgs; [
+    qemu
+    quickemu
+  ];
 
   systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
 }
