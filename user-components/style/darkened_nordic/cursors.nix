8771bb6c4823b15eebf478be2b240b46d522fb40
CloudyChris <a.c.pufu@gmail.com>
2025-06-16 21:07:13 +0300

    forgot to git rm

--- user-components/style/darkened_nordic/cursors.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ...}:
-{
-  home.packages = with pkgs; [
-    nordic-cursor-theme
-  ];
-}
--- user-components/style/darkened_nordic/rofi.nix
+++
@@ -1,7 +0,0 @@
-{ config, ...}:
-{
-  home.file."${config.home.homeDirectory}/.config/rofi/colors.rasi" = {
-    source = ./assets/colors.rasi;
-    force = true;
-  };
-}
