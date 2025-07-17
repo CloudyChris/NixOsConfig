f1a22d25c5b4e2e65476fe8864eeb3d7944f2670
CloudyChris <a.c.pufu@gmail.com>
2025-07-18 02:45:21 +0300

    typo

--- machines/DarkNest/plymouth.nix
+++ machines/DarkNest/plymouth.nix
@@ -12,5 +12,5 @@
   };
 
   # had to add the mkIf because gdm ends up in an infinite cycle if this is true (could add it to lightdm instead of this, maybe, in the future)
-  systemd.services.plymouth-quit.after = lib.mkif (config.services.xserver.displayManager.gdm.enable == false) [ "display-manager.service" ];
+  systemd.services.plymouth-quit.after = lib.mkIf (config.services.xserver.displayManager.gdm.enable == false) [ "display-manager.service" ];
 }
